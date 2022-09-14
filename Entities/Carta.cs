3895b426cc739558ba7b242d382b27fcc9c660c8
Willian Silva <willsoar9620@gmail.com>
2022-09-14 17:28:58 -0300

    Projeto reiniciado. Início da criação da classe Carta

--- Entities/Carta.cs
+++ Entities/Carta.cs
@@ -1,17 +1,19 @@
-using Truco.Entities.Enums;
 using System.Text;
 
 namespace Truco.Entities;
 class Carta {
-    public Valor Valor { get; set; }
-    public string Naipe { get; set; }
+    public int Valor { get; set; }
     public int Pontos { get; set; }
+    public string Naipe { get; set; }
 
-    public Carta (Valor valor) {
-        string[] vectNaipes = {"Espada", "Basto", "Ouro", "Copa"};
-        Valor = valor;
-        Naipe = vectNaipes[(int)Enum.Parse<Naipe>(valor.ToString())];
-        Pontos = (int)Enum.Parse<Ponto>(valor.ToString());
+    public Carta() {
+        Random rand = new Random();
+
+        string[] naipes = new string[4]{"Espada", "Basto", "Ouro", "Copa"};
+
+        Valor = rand.Next(1, 14);
+        Naipe = naipes[rand.Next(0, 3)];
+        Pontos = 0;
     }
 
     public override string ToString() {
@@ -19,18 +21,7 @@ class Carta {
 
         sb.AppendLine("Valor: " + Valor);
         sb.AppendLine("Naipe: " + Naipe);
-        sb.AppendLine("Pontos: " + Pontos);
 
         return sb.ToString();
-    }
-
-    public override bool Equals(object obj) {
-        if (!(obj is Carta)) {
-            return false;
-        }
-
-        Carta outra = obj as Carta;
-
-        return Valor.Equals(outra.Valor);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,38 +1,10 @@
 ﻿using Truco.Entities;
-using Truco.Entities.Enums;
 
 namespace Truco;
 class Program {
     static void Main(string[] args) {
-        // Random rand = new Random();
-        // Carta c1 = new Carta((Valor)rand.Next(1, 14));
-        // Console.WriteLine(c1);
+        Carta c1 = new Carta();
 
-        // List<Carta> cartas = new List<Carta>();
-        // for (int i = 0; i<=40; i++) {
-        //     cartas.Add(new Carta((Valor)rand.Next(1, 14)));
-        // }
-
-        // foreach (Carta c in cartas) {
-        //     Console.WriteLine(c);
-        // }
-
-
-        // string teste = "Willian Eduardo Soares da Silva";
-
-        // bool contem = teste.Contains("Willian");
-
-        // Console.WriteLine(contem);
-
-
-        Jogador j1 = new Jogador();
-
-        Console.WriteLine(j1);
-
-        // Carta c1 = new Carta((Valor)1);
-        // Carta c2 = new Carta((Valor)2);
-        // Carta c3 = new Carta((Valor)1);
-
-        // Console.WriteLine(c1.Equals(c3));
+        Console.WriteLine(c1);
     }
 }
